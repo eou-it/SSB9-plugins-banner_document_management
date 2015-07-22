a305e62c83e46d14ef1169eaa0610be659a6352f
Mahaboob Subahan <mahaboob.subahan@ellucian.com>
2015-07-22 16:58:35 -0400

    Classes refactored for Upload

--- grails-app/services/net/hedtech/banner/imaging/BdmAttachmentService.groovy
+++ grails-app/services/net/hedtech/banner/imaging/BdmAttachmentService.groovy
@@ -8,6 +8,7 @@ import net.hedtech.banner.exceptions.BusinessLogicValidationException
 import net.hedtech.banner.service.ServiceBase
 import net.hedtech.bdm.exception.BdmsException
 import net.hedtech.bdm.services.BDMManager
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 import org.json.JSONObject
 
 import javax.xml.ws.WebServiceException
@@ -15,6 +16,31 @@ import javax.xml.ws.WebServiceException
 class BdmAttachmentService extends ServiceBase {
 
     static transactional = true
+
+    def createBDMLocation(file)
+    {
+        def map = [:]
+        File fileDest
+        String fileName
+
+        String tempPath =  ConfigurationHolder.config.bdm.file.location
+
+        fileName = file.getOriginalFilename()
+        String hashedName = java.util.UUID.randomUUID().toString()
+
+        File userDir = new File(tempPath, hashedName)
+        userDir.mkdir()
+
+        fileDest = new File(userDir, fileName)
+        file.transferTo(fileDest)
+
+        def absoluteFileName = fileDest.getAbsolutePath()
+        map.absoluteFileName = absoluteFileName
+        map.userDir = userDir
+        map.fileName = fileName
+
+        return map
+    }
 
     /**
      * This method is used to create Position Description Comment for a posDescId
@@ -24,11 +50,17 @@ class BdmAttachmentService extends ServiceBase {
      */
     def createDocument(JSONObject bdmParams, String filename, JSONObject docAttributes, String vpdiCode ) throws BdmsException{
 
+        try {
+            def bdm = new BDMManager();
 
-        def bdm = new BDMManager();
-
-        bdm.uploadDocument(bdmParams, filename, docAttributes, vpdiCode);
-
+            bdm.uploadDocument(bdmParams, filename, docAttributes, vpdiCode);
+        }    catch (BdmsException bdme) {
+            if(bdme?.cause?.toString()?.contains("Invalid index value"))
+            {
+                throw new ApplicationException(BdmAttachmentService, new BusinessLogicValidationException("default.invalid.type.exception", []))
+            }
+            throw new ApplicationException(BdmAttachmentService, new BusinessLogicValidationException("default.BdmAttachmentService", []))
+        }
 
     }
 
@@ -39,19 +71,32 @@ class BdmAttachmentService extends ServiceBase {
             def bdm = new BDMManager();
 
             bdm.getDocuments(bdmParams, queryCriteria, vpdiCode);
-        }catch(WebServiceException e)
-            {
-                log.error('BdmAttachmentService',e)
-                throw new BdmsException( 'BdmAttachmentService', e )
+        } catch (BdmsException bdme) {
+                if(bdme.getCause()?.toString()?.contains("Invalid index value"))
+                {
+                    throw new ApplicationException(BdmAttachmentService, new BusinessLogicValidationException("default.invalid.type.exception", []))
+                }
+                throw new ApplicationException(BdmAttachmentService, new BusinessLogicValidationException("default.BdmAttachmentService", []))
             }
+        catch(WebServiceException e)
+        {
+            log.error('BdmAttachmentService',e)
+            throw new BdmsException( 'BdmAttachmentService', e )
+        }
     }
 
     def deleteDocument(JSONObject bdmParams, List docIds, String vpdiCode ) throws BdmsException{
 
         def bdm = new BDMManager();
-
-        bdm.deleteDocument (bdmParams, docIds, vpdiCode);
-
+        try {
+            bdm.deleteDocument(bdmParams, docIds, vpdiCode);
+        } catch (BdmsException bdme) {
+            if(bdme.getCause()?.toString()?.contains("Invalid index value"))
+            {
+                throw new ApplicationException(BdmAttachmentService, new BusinessLogicValidationException("default.invalid.type.exception", []))
+            }
+            throw new ApplicationException(BdmAttachmentService, new BusinessLogicValidationException("default.BdmAttachmentService", []))
+        }
     }
 
     def listDocuments(JSONObject bdmParams, JSONObject queryCriteria, String vpdiCode ) throws BdmsException{
