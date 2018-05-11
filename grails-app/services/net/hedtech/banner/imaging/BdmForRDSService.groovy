af20af1df5189d6c8c90ec0dd44fef34a1873c33
Deepti Mullur <Deepti.Mullur@ellucian.com>
2018-05-11 11:07:26 +0530

    minor change

--- grails-app/services/net/hedtech/banner/imaging/BdmForRDSService.groovy
+++ grails-app/services/net/hedtech/banner/imaging/BdmForRDSService.groovy
@@ -39,22 +39,23 @@ class BdmForRDSService {
             //Passing decrypted value to generateCredential function
             else if (s.equals("f_generate_credential")) {
                 parameter1 = (String) params.get("username");
+				sql.call("{ ? = call eoksecr.f_gspcrpu_decrypt(?)}", [Sql.VARCHAR, parameter1])
+                        { result1 -> parameter1 = result1 }
                 parameter2 = (String) params.get("password");
+				sql.call("{ ? = call eoksecr.f_gspcrpu_decrypt(?)}", [Sql.VARCHAR, parameter2])
+                        { result1 -> parameter2 = result1 }
                 parameter3 = (String) params.get("key");
+                   sql.call("{ ? = call eoksecr.f_gspcrpu_decrypt(?)}", [Sql.VARCHAR, parameter3])
+                        { result1 -> parameter3 = result1 }
                 
                 result = wxc.generateCredentials(parameter1, parameter2, parameter3)
             }
             //Passing decrypted value to generateParameter function
             else if (s.equals("f_paras")) {
                 parameter1 = (String) params.get("username");
-                sql.call("{ ? = call eoksecr.f_gspcrpu_decrypt(?)}", [Sql.VARCHAR, parameter1])
-                        { result1 -> parameter1 = result1 }
                 parameter2 = (String) params.get("password");
-                sql.call("{ ? = call eoksecr.f_gspcrpu_decrypt(?)}", [Sql.VARCHAR, parameter2])
-                        { result1 -> parameter2 = result1 }
-                parameter3 = (String) params.get("key");
-                sql.call("{ ? = call eoksecr.f_gspcrpu_decrypt(?)}", [Sql.VARCHAR, parameter3])
-                        { result1 -> parameter3 = result1 }
+				parameter3 = (String) params.get("key");
+             
                 result = wxc.generateParameters(parameter1, parameter2, parameter3)
             }
 
